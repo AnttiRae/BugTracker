10ae97e3c780330f81fa864e777a6f159d1a388e
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-02-29 12:03:53 +0200

    add WIP PUT functionality for a single bug, add HomeView class for homepage

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 from .forms import BugForm
@@ -14,23 +15,20 @@ class BugView(View):
 
 
     def get(self, request, *args, **kwargs):
-        print('GET jou')
         form = self.form_class(initial=self.initial)
         bugs = Bug.objects.all()
-        print(bugs)
         return render(request, 'bugs/bugs.html', {'form': form, 'bugs': bugs})
 
     ## new bug posting
     def post(self, request, *args , **kwargs):
-        print('POST jou')
         form = self.form_class(request.POST)
-        print(request.POST)
         if form.is_valid():
             bug = request.POST.dict()
             del bug['csrfmiddlewaretoken']
             bug['reported_by'] = request.user
-            Bug.objects.create(**bug)
-            return HttpResponse('Jihuu')
+            bug = Bug.objects.create(**bug)
+            bug.refresh_from_db()
+            return redirect(f'/bugs/{bug.pk}')
         else:
             return HttpResponse('ei jihuu')
 
@@ -38,15 +36,32 @@ class BugView(View):
 class singleBugView(View):
 
     def get(self, request, *args, **kwargs):
-        print(kwargs['pk'])
-        bug = Bug.objects.get(pk=kwargs['pk'])
+        try:
+            bug = Bug.objects.get(pk=kwargs['pk'])
+        except ObjectDoesNotExist as error:
+            return HttpResponse(error)
         return render(request, 'bugs/singleBug.html', {'bug': bug})
+    
+    def put(self, request, *args, **kwargs):
+        bug = Bug.objects.get(pk=kwargs['pk'])
+        print(bug)
+        return HttpResponse('PUTattu')
 
 class ProfileView(View):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            print('request', request.user)
             current_user = User.objects.get(username=request.user)
             return render(request, 'profile/profile.html', {'user': current_user})
         return render(request, 'profile/profile.html')
+
+class HomeView(View):
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            current_user = User.objects.get(username=request.user)
+            all_bugs = Bug.objects.all()
+            bugs_by_user = all_bugs.filter(reported_by=current_user)
+            
+            return render(request, 'home.html', {'current_user': current_user, 'all_bugs': all_bugs ,'bugs_by_user': bugs_by_user})
+        return render(request, 'home.html')
