771ac57a8f68c166ca78850291b81443879f2323
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-03-12 10:56:09 +0200

    reformat code

--- app/forms.py
+++ app/forms.py
@@ -7,10 +7,22 @@ class BugForm(forms.Form):
         ('Low', 'Low'),
     )
 
-    title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label='Title', max_length=80)
-    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), label='Description', max_length=10000)
-    priority = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=PRIORITIES)
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class':'form-control'}),
+        label='Title', max_length=80)
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'class': 'form-control'}),
+        label='Description', max_length=10000)
+    priority = forms.ChoiceField(
+        widget=forms.Select(
+            attrs={'class': 'form-control'}), 
+        choices=PRIORITIES)
 
 class CommentForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 2}), label='Comment', max_length=512)
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'class': 'form-control', 'rows': 2}),
+        label='Comment', max_length=512)
     
